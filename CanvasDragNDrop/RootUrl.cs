b7946d0506078f8df12f6d15dbe4fd677b12d049
redcake36 <kan.denis10@gmail.com>
2023-04-20 22:01:13 +0300

    work prototip, after meeting

---
+++ CanvasDragNDrop/RootUrl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanvasDragNDrop
+{
+    public static class RootUrl
+    {
+        public static string rootServer = "https://b57a-95-220-40-200.ngrok-free.app";
+    }
+}
